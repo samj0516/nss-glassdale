4bfe8a611503022e965fee5048e2b43bcc7dcab1
samj0516 <samj0516@gmail.com>
2021-02-11 16:23:26 -0600

    completed chapter 13

--- scripts/main.js
+++ scripts/main.js
@@ -7,12 +7,17 @@ import './notes/NoteList.js'
 import './alibis/AlibiProvider.js'
 import { ShowWitnessesButton } from './witnesess/ShowWitnessButton.js'
 import { WitnessList } from './witnesess/WitnessList.js'
+import { getCriminals } from './criminals/CriminalDataProvider.js'
+
+getCriminals()
+    .then(NoteForm)
 
 
 CriminalList()
 ConvictionSelect()
 OfficerSelect()
-NoteForm()
+
+// NoteForm()
 ShowNoteButton()
 ShowWitnessesButton()
 // WitnessList()
--- scripts/notes/NoteList.js
+++ scripts/notes/NoteList.js
@@ -1,5 +1,6 @@
 import { getNotes, useNotes } from "./NoteDataProvider.js";
 import { NoteHTMLConverter } from "./Note.js";
+import { getCriminals, useCriminals } from '../criminals/CriminalDataProvider.js'
 
 // Query the DOM for the element that your notes will be added to 
 const contentTarget = document.querySelector(".noteListContainer")
@@ -10,25 +11,54 @@ eventHub.addEventListener("showNotesClicked", customEvent => {
     NoteList()
 })
 
-const render = (noteArray) => {
-    const allNotesConvertedToStrings = noteArray.map(noteObject => {
-        return NoteHTMLConverter(noteObject)
 
+const render = (noteArray, criminalArray) => {
+    const allNotesToStrings = noteArray.map(noteObject => {
+        const relatedCriminal = criminalArray.find(criminal => criminal.id === noteObject.criminalId)
+        
+        return NoteHTMLConverter(noteObject, relatedCriminal)
+        debugger
     }).join("")
-
-    contentTarget.innerHTML = `
-        <h3>Case Notes</h3>
+    contentTarget.innerHTML = `<h3>Case Notes</h3>
         <section class="notesList">
-        ${allNotesConvertedToStrings}
+            ${allNotesToStrings}
         </section>`
+
+
+     
 }
+
+
+
+
+
+
+
+// const render = (noteArray, criminalArray) => {
+//     contentTarget.innerHTML = noteArray.map(noteObject => {
+//         const relatedCriminal = criminalArray.find(criminal => criminal.id === parseInt(noteObject.criminalId))
+//         console.log(relatedCriminal)
+//         return `
+//         <h3>Case Notes</h3>
+//         <section class="notesList">
+//             <div class="note__date">Date: ${ new Date(noteObject.date).toLocaleDateString('en-US')  }</div>
+//             <div class="note__criminal">Suspect: ${ relatedCriminal.name }</div>
+//             <div class="note__text">Note: ${ noteObject.note }</div>
+//         </section>`
+
+//     }).join("")
+
+     
+// }
 
 // Standard list function you're used to writing by now. BUT, don't call this in main.js! Why not?
 export const NoteList = () => {
-    getNotes()
+    getCriminals()
+        .then(getNotes)
         .then(() => {
             const allNotes = useNotes()
-            render(allNotes)
+            const criminals = useCriminals()
+            render(allNotes, criminals)
         })
 }
 eventHub.addEventListener("noteStateChanged", event => {
